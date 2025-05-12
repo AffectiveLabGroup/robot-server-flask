69d257b735e1349287a5981ee016a437d3265f29
AffectiveLabGroup <affectivelabgroup@gmail.com>
2025-05-12 12:54:05 +0200

    up

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", logger=True, engineio_logger=True)
+
 
 # Almacén de mensajes
 messages = {
